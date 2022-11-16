996c6632998f21d74e2b101ef053568d48aa7844
neumanv <guillermoanv25@gmail.com>
2022-11-16 16:45:09 +0100

    Cambios

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,18 @@
 "use strict"
 
+//----------------- NAVBAR RESPONSIVE ----------------------------------------------
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.className === "links") {
+        x.className += " responsive";
+    } else {
+        x.className = "links";
+    }
+}
+
+//----------------- CUENTA ATRÁS ---------------------------------------------------
+
 document.addEventListener('DOMContentLoaded', () => { 
 
     // VARIABLES
